afca327743eeb44d9d4a75fe75199b4a8f483c45
Bruno Borin <psclaram@gmail.com>
2020-09-06 10:41:08 -0300

    javascript

---
+++ dom/index.js
@@ -0,0 +1,18 @@
+function soma(n1,n2){
+    return n1+n2;
+}
+
+var total = soma(1,2);
+console.log(total)
+
+const nsoma = function (n1,n2){
+    return n1+n2;
+}
+
+var total2 = nsoma(1,2);
+console.log(total2);
+
+const nsoma3 = (n1,n2) =>
+ n1 + n2;
+var total3 = nsoma3(4,3);
+console.log(total3);
